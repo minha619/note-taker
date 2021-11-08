cb046a545cddb8168e1086c8408c672d49e1a676
minha619 <minha619@gmail.com>
2021-11-07 23:22:31 -0500

    update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,53 @@
-const { application } = require('express');
-const db = require('../db/db');
+const fs = require("fs");
+let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 
-module.exports = (app) => {
-    app.get('/api/notes', (req, res) => {
-        res.json(db);
-    });
+module.exports = function(app) {
 
-
-    app.post('/api/notes', (req, res) => {
-        db.push(req.body);
-        res.json(true);
-    });
-
-    app.delete('/api/notes/:id', (req, res) => {
+    app.get("/api/notes", function(req, res) {
+       
+        res.json(data);
 
     });
 
-};
+    app.get("/api/notes/:id", function(req, res) {
+
+        res.json(data[Number(req.params.id)]);
+
+    });
+
+
+    app.post("/api/notes", (req, res) => {
+
+        let newNote = req.body;
+        let savedNoteId = (data.length).toString();
+       
+        newNote.id = savedNoteId;
+        data.push(newNote);
+        
+        fs.writeFileSync("./db/db.json", JSON.stringify(data), function(err) {
+            if (err) throw (err);        
+        }); 
+
+        res.json(data);    
+
+    });
+
+    
+    app.delete("/api/notes/:id", (req, res) => {
+
+        let noteId = req.params.id;
+        let newId = 0;
+
+        data = data.filter(currentNote => {
+           return currentNote.id != noteId;
+        });
+        for (currentNote of data) {
+            currentNote.id = newId.toString();
+            newId++;
+        }
+        fs.writeFileSync("./db/db.json", JSON.stringify(data));
+        res.json(data);
+    }); 
+
+}
